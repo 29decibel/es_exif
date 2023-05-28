74c62845406471e5a30d7037658cfd1b62fc48c8
29decibel <m@idots.me>
2023-05-28 13:51:11 -0700

    test for date parsing

--- test/es_exif_test.exs
+++ test/es_exif_test.exs
@@ -6,4 +6,9 @@ defmodule EsExifTest do
     {:ok, result} = EsExif.parse("./test_photos/_DSF2568.JPG")
     assert Map.get(result, "LensModel") == "XF16mmF1.4 R WR"
   end
+
+  test "return parsed date for DateTimeOriginal" do
+    {:ok, result} = EsExif.parse("./test_photos/_DSF2568.JPG")
+    assert Map.get(result, "DateTimeOriginal") == ~N[2021-10-02 11:24:03]
+  end
 end
